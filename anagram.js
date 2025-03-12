f1aeeb8d45eb6f67530c5a42969527cd4649d80a
Amit Verma <amitjsr8786@gmail.com>
2025-03-13 00:48:07 +0530

    find anagram

---
+++ anagram.js
@@ -0,0 +1,6 @@
+function anagram(str1,str2){
+    let sortStr1=str1.split("").sort().join("");
+    let sortStr2=str2.split("").sort().join("")
+    return sortStr1===sortStr2
+}
+console.log(anagram("silent","listen"))
