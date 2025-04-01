dcdb8eb8dfefd3550616ad21bbe4e89c9567589f
Amit Verma <amitjsr8786@gmail.com>
2025-04-01 12:52:28 +0530

    find vowels

---
+++ vowels.js
@@ -0,0 +1,11 @@
+function getVowels(str){
+    let vowels=["a","e","i","o","u"]
+    let count=0;
+    for(let char of str.toLowerCase()){
+      if(vowels.includes(char)){
+        count++
+      }
+    }
+    return count;
+  }
+  console.log(getVowels("Hello"))
