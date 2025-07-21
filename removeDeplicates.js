d3397a912da9d49cc702b80b97669d7765a43320
Rishabh Kumar <rishabh2807s@gmail.com>
2025-07-21 23:15:04 +0530

    remove-duplicates

--- removeDeplicates.js
+++ removeDeplicates.js
@@ -1,10 +1,10 @@
-function removeDuplicate(nums){
-    for(let i=0;i<nums.length-1;i++){
-        if(nums[i]===nums[i+1]){
-            nums.splice(i+1,1);
-            i--;
-        }
+function removeDuplicate(nums) {
+  for (let i = 0; i < nums.length - 1; i++) {
+    if (nums[i] === nums[i + 1]) {
+      nums.splice(i + 1, 1);
+      i--;
     }
-    return nums.length
+  }
+  return nums.length;
 }
-console.log(removeDuplicate([1,1,2])) 
+console.log(removeDuplicate([1, 1, 2]));
